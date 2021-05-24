fbc463f52c22e91a47fd522ada346b4c858592ae
Hasar Ali <hasar.ali@protonmail.com>
2021-05-24 00:36:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 
 function totalBatteries() {
-  return reduce( batteryBatches, (total, battery) => battery , 0)
+  return reduce( batteryBatches, (total, battery) => total + battery , 0)
 }
